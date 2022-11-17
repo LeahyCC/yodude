ac51d5c27b9b9112bdf8fbd2ad8124ef8a4144be
Colin Leahy <ccleahy00@hotmail.com>
2022-11-16 20:36:54 -0700

    update import name

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import classNames from 'classNames'
+import classNames from 'classnames'
 import { Link } from 'wouter'
 import * as style from './navigation.styles'
 import rocket from './rocket.png'
