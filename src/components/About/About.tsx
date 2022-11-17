150f845e9def050f64d89ae6b83cef38abb46f05
Colin Leahy <ccleahy00@hotmail.com>
2022-11-16 20:21:54 -0700

    update styles

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,7 @@ import * as styles from './about.styles'
 const About = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>About me</h1>
+      <h1 className={styles.heading}>About mee</h1>
     </div>
   )
 }
