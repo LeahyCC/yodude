302f3e3e51ccff05e3107f592a9a805b84d62911
Colin Leahy <ccleahy00@hotmail.com>
2022-11-16 20:12:27 -0700

    update styles

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,7 @@
 import * as styles from './home.styles'
 
 const Home = () => {
-  return (
-    <h1 className={styles.heading}>
-      Hi! <br /> I`m Colin, <br /> Frontend Engineer
-    </h1>
-  )
+  return <h1 className={styles.heading}>Hi!</h1>
 }
 
 export default Home
