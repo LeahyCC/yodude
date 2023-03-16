f55573e5d7c9728f3adaf95afb7b3184009b0548
Colin Leahy <ccleahy2020@gmail.com>
2023-03-15 21:30:23 -0700

    test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,6 +3,8 @@ import { render, screen, act, fireEvent } from '@testing-library/react'
 import { expect, beforeEach, describe, test } from 'vitest'
 import App from './App'
 
+// test
+
 describe('App', () => {
   beforeEach(async () => {
     await act(async () => {
