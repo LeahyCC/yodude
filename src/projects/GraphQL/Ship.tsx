ee247400a1ff6da73052a76a20d4bd82e3748e61
Colin Leahy <ccleahy2020@gmail.com>
2023-03-20 10:04:25 -0700

    comments

--- src/projects/GraphQL/Ship.tsx
+++ src/projects/GraphQL/Ship.tsx
@@ -1,13 +1,17 @@
 import { FragmentType, useFragment } from './gql'
 import { ShipFragment } from './useStarShips'
 
+/**
+ * This component isn't being used in the app,
+ * but it's here to show how to use the fragment in a component
+ */
+
 export const Ship = (props: { ship: FragmentType<typeof ShipFragment> }) => {
   const ship = useFragment(ShipFragment, props.ship)
   return (
     <div>
       <h3>Name: {ship.name}</h3>
       <p>Model: {ship.model}</p>
-      <br />
     </div>
   )
 }
